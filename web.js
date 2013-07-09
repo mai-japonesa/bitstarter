efbd38266c8f8121f30df47bfc7524d2ef74b3fa
mai <maytkwtsh@gmail.com>
2013-07-09 21:30:57 +0900

    changed

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('index.html');
+  file=fs.readFileSync('index.html');
+  response.send(file);
 });
 
 var port = process.env.PORT || 5000;
